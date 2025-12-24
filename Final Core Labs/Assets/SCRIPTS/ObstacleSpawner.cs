ec48745bb66972a04cadee38b9ec9279680cf434
bunniay <bunniaybb@gmail.com>
2025-12-24 08:28:55 -0500

    what the fuck

--- Final Core Labs/Assets/SCRIPTS/ObstacleSpawner.cs
+++ Final Core Labs/Assets/SCRIPTS/ObstacleSpawner.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    public float spawnRate = 7f;
+    public float spawnRate = 5f;
 
-    private float[] lanes = { 2f, 0f, -2f };
+    private readonly float[] lanes = { 2f, 0f, -2f };
 
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", 5f, spawnRate);
+        InvokeRepeating(nameof(SpawnObstacle), 1f, spawnRate);
     }
 
     void SpawnObstacle()
     {
         float laneY = lanes[Random.Range(0, lanes.Length)];
-        Vector3 spawnPos = new Vector3(transform.position.x, laneY, 0);
+        Vector3 spawnPos = new(transform.position.x, laneY, 0);
 
         Instantiate(obstaclePrefab, spawnPos, Quaternion.identity);
     }
